88a74f5e45bc114a504f31177e53a95f51ccf5ef
joetam <1564331+joetam@users.noreply.github.com>
2026-08-08 10:16:22 -0700

    Add Cappy cask

---
+++ Casks/cappy.rb
@@ -0,0 +1,14 @@
+cask "cappy" do
+  version "0.1.7"
+  sha256 "4a3bf3daa2c275a89e632a93061785190707ccec96d9c0f192527a0d4654443c"
+
+  url "https://github.com/joetam/cappy/releases/download/v#{version}/Cappy-#{version}-macos-arm64.zip"
+  name "Cappy"
+  desc "Track usage limits across coding accounts"
+  homepage "https://github.com/joetam/cappy"
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :sonoma"
+
+  app "Cappy.app"
+end
---
+++ README.md
@@ -0,0 +1,7 @@
+# Homebrew tap for Cappy
+
+Install [Cappy](https://github.com/joetam/cappy):
+
+```sh
+brew install --cask joetam/tap/cappy
+```
